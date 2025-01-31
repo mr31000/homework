399f35fdca36415b48e1119ced541f70f0515c35
mr31000 <mr3100@bs.amalnet.k12.il>
2025-01-31 07:30:04 +0200

    סיום

--- app/src/main/java/com/example/homework/MainActivity.java
+++ app/src/main/java/com/example/homework/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.homework;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,9 +11,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 public void btnClick (View view)
 {
+    ImageView iv1 = (ImageView) findViewById(R.id.iv);
+    Button btn = (Button) findViewById(R.id.btn);
+    Random random =new Random();
+    int randomNumber = random.nextInt(3) + 1;
+    btn.setText(String.valueOf(randomNumber));
+
+    if (randomNumber == 1) {
+        iv1.setImageResource(R.drawable.image1);
+    } else if (randomNumber == 2) {
+        iv1.setImageResource(R.drawable.image2);
+    } else if (randomNumber == 3) {
+        iv1.setImageResource(R.drawable.image3);
+    }
+
+    iv1.setVisibility(View.VISIBLE);
 
 }
     @Override
